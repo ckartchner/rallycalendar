0f7bd7bab062899ea552bccddc4cfc247db1b1d3
Carlin Kartchner <carlinsjunk2@gmail.com>
2020-08-11 10:23:58 -0400

    Highlight event status

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ function parseDate(dateString) {
   const year = dateParts[0];
   const month = dateParts[1] - 1;
   const day = dateParts[2];
-  const formatOptions = {weekday: 'short', day: 'numeric', month: 'short'};
-  const dateObj = new Date(year, month, day);
-  // Handle locales not usable by toLocaleDateString
+  const rawDateObj = new Date(year, month, day);
+  return rawDateObj;
+}
+
+function formatDate(dateObj) {
   let formattedDate = 'undefined';
+  const formatOptions = {weekday: 'short', day: 'numeric', month: 'short'};
+  // Handle locales not usable by toLocaleDateString
   try {
     formattedDate = dateObj.toLocaleDateString(locale, formatOptions);
   } catch(err) {
@@ -20,18 +24,46 @@ function parseDate(dateString) {
       formattedDate = dateObj.toLocaleDateString('en-us', formatOptions);
     }
   }
-  return formattedDate;
+  return formattedDate
 }
 
 function parseCal(data) {
   let events = []
   data.rallyEvents.items.forEach(event => {
     eventDays = event.eventDays
-    startDate = parseDate(eventDays[0]['eventDay']);
-    endDate = parseDate(eventDays[eventDays.length - 1]['eventDay']);
+    rawStartDate = parseDate(eventDays[0]['eventDay']);
+    rawEndDate = parseDate(eventDays[eventDays.length - 1]['eventDay']);
+    startDate = formatDate(rawStartDate);
+    endDate = formatDate(rawEndDate);
     eventInfo = `https://www.wrc.com${event.pageInfo['url']}`
-    events.push({'name': event.name, 'startDate': startDate, 'endDate': endDate, 'eventInfo': eventInfo})
+    events.push({'name': event.name, 'startDate': startDate, 'endDate': endDate, 
+                 'eventInfo': eventInfo, 'rawStartDate': rawStartDate, 
+                 'rawEndDate': rawEndDate});
   })
+  // Set event status
+  current_date = new Date()
+  next_event_index = undefined;
+  next_event_diff = new Date(current_date.getFullYear() + 5, 0) - current_date;
+  for (let i = 0; i < events.length; i++) {
+    event = events[i]
+    if (event.rawEndDate < current_date) {
+      events[i].status = 'past';
+    } else if (event.rawStartDate > current_date) {
+      events[i].status = 'future';
+    } else if (event.rawStartDate <= current_date && event.rawEndDate >= current_date) {
+      event[i].status = 'active';
+    } else {
+      event[i].status = 'unknown';
+    }
+    time_till_event = event.rawEndDate - current_date
+    if (time_till_event < next_event_diff && time_till_event > 0) {
+      next_event_index = i;
+      next_event_diff = time_till_event;
+    }
+  }
+  if (next_event_index !== 'undefined') {
+    events[next_event_index].status = 'next';
+  }
   return events;
 }
 
