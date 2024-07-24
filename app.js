27e2ef08cb3aff85990e88b856849f91b1b83272
Carlin Kartchner <carlin.kartchner@gmail.com>
2024-07-23 23:30:17 -0400

    restore status highlighting + lint

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const currentTime = new Date()
-const currentYear = currentTime.getFullYear()
+const currentTime = new Date();
+const currentYear = currentTime.getFullYear();
 const calendar_api = `https://api.wrc.com/content/filters/calendar?championship=wrc&origin=vcms&year=${currentYear}`;
 const locale = window.navigator.languages;
 
 function parseDate(dateString) {
-  const dateParts = dateString.split('-');
+  const dateParts = dateString.split("-");
   const year = dateParts[0];
   const month = dateParts[1] - 1;
   const day = dateParts[2];
@@ -13,25 +13,25 @@ function parseDate(dateString) {
 }
 
 function formatDate(dateObj) {
-  let formattedDate = 'undefined';
-  const formatOptions = {weekday: 'short', day: 'numeric', month: 'short'};
+  let formattedDate = "undefined";
+  const formatOptions = { weekday: "short", day: "numeric", month: "short" };
   // Handle locales not usable by toLocaleDateString
   try {
     formattedDate = dateObj.toLocaleDateString(locale, formatOptions);
-  } catch(err) {
-    try{
+  } catch (err) {
+    try {
       formattedDate = dateObj.toLocaleDateString(locale[0], formatOptions);
-    } catch{
+    } catch {
       // if all else fails default to en-us locale
-      formattedDate = dateObj.toLocaleDateString('en-us', formatOptions);
+      formattedDate = dateObj.toLocaleDateString("en-us", formatOptions);
     }
   }
-  return formattedDate
+  return formattedDate;
 }
 
 function parseCal(data) {
-  let events = []
-  data.content.forEach(event => {
+  let events = [];
+  data.content.forEach((event) => {
     // eventDays = event.eventDays
     // rawStartDate = parseDate(eventDays[0]['eventDay']);
     // rawEndDate = parseDate(eventDays[eventDays.length - 1]['eventDay']);
@@ -41,52 +41,60 @@ function parseCal(data) {
     // if (event.pageInfo !== null){
     //   eventInfo = `https://www.wrc.com${event.pageInfo['url']}`
     // }
-    // events.push({'name': event.title, 'startDate': startDate, 'endDate': endDate, 
-    //              'eventInfo': event.Description, 'rawStartDate': rawStartDate, 
+    // events.push({'name': event.title, 'startDate': startDate, 'endDate': endDate,
+    //              'eventInfo': event.Description, 'rawStartDate': rawStartDate,
     //              'rawEndDate': rawEndDate});
     events.push({
-      'name': event.title, 
-      'startDate': event.startDateLocal, 
-      'endDate': event.endDateLocal, 
-      'eventInfo': event.description
+      name: event.title,
+      startDate: event.startDateLocal,
+      endDate: event.endDateLocal,
+      rawStartDate: new Date(event.startDateLocal),
+      rawEndDate: new Date(event.endDateLocal),
+      eventInfo: event.description,
     });
-  })
-  // // Set event status
-  // current_date = new Date()
-  // next_event_index = undefined;
-  // active_event = false;
-  // next_event_diff = new Date(current_date.getFullYear() + 5, 0) - current_date;
-  // for (let i = 0; i < events.length; i++) {
-  //   event = events[i]
-  //   if (event.rawEndDate < current_date) {
-  //     events[i].status = 'past';
-  //   } else if (event.rawStartDate > current_date) {
-  //     events[i].status = 'future';
-  //   } else if (event.rawStartDate <= current_date && event.rawEndDate >= current_date) {
-  //     events[i].status = 'active';
-  //     active_event = true;
-  //   } else {
-  //     events[i].status = 'unknown';
-  //   }
-  //   time_till_event = event.rawEndDate - current_date
-  //   if (time_till_event < next_event_diff && time_till_event > 0) {
-  //     next_event_index = i;
-  //     next_event_diff = time_till_event;
-  //   }
-  // }
-  // noIndexStates = ['undefined', undefined]
-  // if (noIndexStates.includes(next_event_index) === false && active_event === false) {
-  //   events[next_event_index].status = 'next';
-  // }
+  });
+  // Set event status
+  current_date = new Date();
+  next_event_index = undefined;
+  active_event = false;
+  next_event_diff = new Date(current_date.getFullYear() + 5, 0) - current_date;
+  for (let i = 0; i < events.length; i++) {
+    event = events[i];
+    if (event.rawEndDate < current_date) {
+      events[i].status = "past";
+    } else if (event.rawStartDate > current_date) {
+      events[i].status = "future";
+    } else if (
+      event.rawStartDate <= current_date &&
+      event.rawEndDate >= current_date
+    ) {
+      events[i].status = "active";
+      active_event = true;
+    } else {
+      events[i].status = "unknown";
+    }
+    time_till_event = event.rawEndDate - current_date;
+    if (time_till_event < next_event_diff && time_till_event > 0) {
+      next_event_index = i;
+      next_event_diff = time_till_event;
+    }
+  }
+  noIndexStates = ["undefined", undefined];
+  if (
+    noIndexStates.includes(next_event_index) === false &&
+    active_event === false
+  ) {
+    events[next_event_index].status = "next";
+  }
   return events;
 }
 
 let eventTable = new Vue({
-  el: '#vue-scope',
+  el: "#vue-scope",
   data: {
     events: null,
     year: currentYear,
-    state: 'uninitialized',
+    state: "uninitialized",
   },
   async created() {
     const response = await fetch(calendar_api);
@@ -94,9 +102,9 @@ let eventTable = new Vue({
     if (response.ok) {
       data = await response.json();
       this.events = parseCal(data);
-      this.state = 'good_response';
+      this.state = "good_response";
     } else {
-      this.state = 'bad_response';
+      this.state = "bad_response";
     }
-  }
+  },
 });
